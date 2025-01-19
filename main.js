d93d4fbb1885e53359758293f385db4a3409639c
ArtisArtStudio <karaca.aykut@gmail.com>
2025-01-19 23:07:07 +0300

    fixed form

--- main.js
+++ main.js
@@ -270,7 +270,7 @@ var pct =new Array(9);
         //document.getElementById('intro').innerHTML= "This is a gender reveal scratch off for <strong>" + surname + "</strong> family. It contains sound when the gender is revealed. Do you want to continue with sound?";
         document.getElementById('surname').innerHTML= surname;
 
-        //document.getElementById('id01').style.display='block';
+        document.getElementById('id01').style.display='block';
         $('.nosoundbtn').on("click", function (e) {
             document.getElementById('id01').style.display='none';
             nosound=true;
